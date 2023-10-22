c5e093c2a7950bd8219a962b24ffdf2b56065262
Amin7775 <amin.dpi24@gmail.com>
2023-10-23 00:58:05 +0600

    Added get method for view details(products)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -59,6 +59,14 @@ async function run() {
       res.send(result)
     })
     //Add Products section - end
+
+    //view single product from db
+    app.get('/products/:id',async(req,res)=> {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await productCollection.findOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
